6463d08e0e4c6b03fcb4aec0844a4cb292e53085
unknown <johan.morales@saiavh.com>
2018-11-08 16:28:05 -0500

    Se agregan strings para trabajarlas desde un solo archivo, se crea la activity de hourly forecast

---
+++ app/src/main/java/com/example/johanmorales/colorweatherapp/HourlyForecastActivity.java
@@ -0,0 +1,13 @@
+package com.example.johanmorales.colorweatherapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HourlyForecastActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+    }
+}
